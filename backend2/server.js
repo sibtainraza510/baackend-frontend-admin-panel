ebe28b91b437809c1b4ef37bffb6f30bf66be45d
MOHD SIBTAYN RZA <161344158+sibtainraza510@users.noreply.github.com>
2025-05-06 14:46:52 +0530

    Update server.js

--- backend2/server.js
+++ backend2/server.js
@@ -12,8 +12,7 @@ const errorMiddleware = require("./middlewares/error.middleware.js")
 const corsOptions = {
     origin: [
         "http://localhost:5173",
-        "https://mernadmintechsite.netlify.app",
-        "https://baackend-frontend-admin-panel-1.onrender.com"
+        "https://mernadmintechsite.netlify.app"
       ],
     methods : "GET, POST, DELETE, PATCH, PUT, HEAD",
     credentials : true
